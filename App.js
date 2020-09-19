c15821e1b04b700c358fe5f4a5337c5159c098ca
Apichet Sittipon <54353591+dewapichet@users.noreply.github.com>
2020-09-19 15:26:31 +0700

    movie

--- App.js
+++ App.js
@@ -1,21 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import MoviesList from "./screens/MoviesList"
+import MovieDetail from './screens/MovieDetail'
+
+
+const Stack = createStackNavigator();
+
+const Navigation = () => (
+  <NavigationContainer>
+    <Stack.Navigator
+    screenOptions ={
+      {
+        headerStyle:{
+          backgroundColor: 'black'
+        },
+        headerTintColor: 'white'
+      }
+    }>
+      <Stack.Screen
+        name="MoviesList"
+        options={{
+          title:'Movie Show Time Example'
+        }}
+        component={MoviesList}
+      />
+      <Stack.Screen
+        name="MovieDetail"
+        options={{
+          title: null,
+          headerTruncatedBackTitle:null
+        }}
+        component={MovieDetail}
+      />
+    </Stack.Navigator>
+  </NavigationContainer>
+)
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <Navigation/>
     </View>
   );
 }
 
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
   },
 });
